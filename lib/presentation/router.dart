3be385729e08e4103f8cc956b4ee6996aefa39fc
Heshan <heshan.imedha@gmail.com>
2021-11-09 22:32:52 +0530

    bloc provider

--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopping_list/constants/string.dart';
+import 'package:shopping_list/cubit/items_cubit_dart_cubit.dart';
 import 'package:shopping_list/presentation/screens/add_item_screen.dart';
 import 'package:shopping_list/presentation/screens/edit_item_screen.dart';
 import 'package:shopping_list/presentation/screens/shoppinglist_screen.dart';
@@ -8,7 +10,11 @@ class ShoppingListRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => ShoppingListScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (context) => ItemsCubitDartCubit(),
+                  child: ShoppingListScreen(),
+                ));
       case EDIT_ITEM_ROUTE:
         return MaterialPageRoute(builder: (_) => EditItemScreen());
       case ADD_ITEM_ROUTE:
